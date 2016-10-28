5191c3bddd81ae6fcb7ccc8b634e29eff9b7a24c
max marschark <maxmarschark@gmail.com>
2016-10-28 17:02:26 -0400

    changed styling

--- scripts.js
+++ scripts.js
@@ -8,7 +8,7 @@ $(function() {
       if (target.length) {
         $('html, body').animate({
           scrollTop: target.offset().top
-        }, 750);
+        }, 1000);
         return false;
       }
     }
